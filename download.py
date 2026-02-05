a2d298113003c840c4babbf4320230acdec58e21
timuroff12 <ahmadullintimur542@gmail.com>
2026-02-05 12:36:47 +0500

    Add files via upload

---
+++ download.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="Monte Carlo Trader Sim", layout="wide")
+
+st.title("📊 Симуляция Монте-Карло для трейдеров")
+st.markdown("Проверь свою стратегию на прочность прежде, чем рисковать реальными деньгами.")
+
